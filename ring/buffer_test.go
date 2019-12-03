2fef63d43b9c3df9493abf953f5c88fba861174f
Nicola Girardi <nicola@aloc.in>
2019-12-03 21:47:24 +0000

    ring: add quick check test
    Did this as a learning exercise.

--- ring/buffer_test.go
+++ ring/buffer_test.go
@@ -2,9 +2,14 @@ package ring_test
 
 import (
 	"errors"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"math/rand"
+	"os"
+	"reflect"
 	"testing"
+	"testing/quick"
 
 	"github.com/nicolagi/pine/ring"
 	"github.com/stretchr/testify/assert"
@@ -20,36 +25,6 @@ func TestBufferIllegalCapacity(t *testing.T) {
 	buffer, err := ring.NewBuffer(0)
 	require.Nil(t, buffer)
 	require.True(t, errors.Is(err, ring.ErrIllegal))
-}
-
-func TestBufferWriteReadCycles(t *testing.T) {
-	// Get a ring buffer of capacity in 1, ..., 1024
-	capacity := 1 + rand.Uint32()%1024
-	buffer, err := ring.NewBuffer(capacity)
-	require.Nil(t, err)
-
-	for i := 0; i < 1024; i++ {
-		// Generate a random byte slice to store in the buffer, of length in 0, ..., capacity, including full capacity.
-		size := rand.Uint32() % (capacity + 1)
-		value := make([]byte, size)
-		n, err := rand.Read(value)
-		assert.EqualValues(t, size, n)
-		assert.Nil(t, err)
-
-		// Nothing available for reading.
-		assert.EqualValues(t, 0, buffer.Readable())
-
-		// Write and read back.
-		n, err = buffer.Write(value)
-		require.Nil(t, err)
-		require.EqualValues(t, size, n)
-		assert.EqualValues(t, size, buffer.Readable())
-		back := make([]byte, size)
-		n, err = buffer.Read(back)
-		require.Nil(t, err)
-		assert.EqualValues(t, size, n)
-		assert.Equal(t, value, back)
-	}
 }
 
 func TestBufferWriteOverflow(t *testing.T) {
@@ -76,4 +51,124 @@ func TestBufferReadOverflow(t *testing.T) {
 	assert.Equal(t, 5, n)
 	assert.Equal(t, append([]byte("hello"), 0, 0, 0), back)
 	assert.True(t, errors.Is(err, ring.ErrOverflow))
+}
+
+type opType uint8
+
+const (
+	opRead  opType = 0
+	opWrite opType = 1
+)
+
+type op struct {
+	t opType // opRead or opWrite
+	n int    // for read: how many bytes to read
+	p []byte // for write: the bytes to write
+}
+
+func (op *op) String() string {
+	switch op.t {
+	case opRead:
+		return fmt.Sprintf("read of %d bytes", op.n)
+	case opWrite:
+		return fmt.Sprintf("write of %d bytes, %.8x", len(op.p), op.p)
+	default:
+		panic("unknown op type")
+	}
+}
+
+type opOutput struct {
+	n   int    // for read and write: the number of bytes read/written
+	err error  // for read and write
+	p   []byte // only for read: the bytes that were read
+}
+
+func (op *op) applyToRingBuffer(rb *ring.Buffer) *opOutput {
+	switch op.t {
+	case opRead:
+		p := make([]byte, op.n)
+		n, err := rb.Read(p)
+		return &opOutput{n: n, err: err, p: p}
+	case opWrite:
+		n, err := rb.Write(op.p)
+		return &opOutput{n: n, err: err}
+	default:
+		panic("unknown op type")
+	}
+}
+
+type file struct {
+	osf         *os.File
+	readOffset  int64
+	writeOffset int64
+}
+
+func (op *op) applyToFile(f *file) *opOutput {
+	switch op.t {
+	case opRead:
+		p := make([]byte, op.n)
+		n, err := f.osf.ReadAt(p, f.readOffset)
+		f.readOffset += int64(n)
+		return &opOutput{n: n, err: err, p: p}
+	case opWrite:
+		n, err := f.osf.WriteAt(op.p, f.writeOffset)
+		f.writeOffset += int64(n)
+		return &opOutput{n: n, err: err}
+	default:
+		panic("unknown op type")
+	}
+}
+
+func TestRingBufferWhatYouWriteIsWhatYouRead(t *testing.T) {
+	bufferSizes := []int{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384}
+	for _, bufferSize := range bufferSizes {
+		t.Run(fmt.Sprintf("buffer-size-%d", bufferSize), func(t *testing.T) {
+			availableForReading := 0
+			buffer, err := ring.NewBuffer(uint32(bufferSize))
+			if err != nil {
+				t.Fatal(err)
+			}
+			f, err := ioutil.TempFile("", "pine-ring-")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer func() {
+				_ = f.Close()
+				_ = os.Remove(f.Name())
+			}()
+			refImpl := &file{osf: f}
+			err = quick.CheckEqual(func(op *op) *opOutput {
+				t.Log(op)
+				return op.applyToRingBuffer(buffer)
+			}, func(op *op) *opOutput {
+				return op.applyToFile(refImpl)
+			}, &quick.Config{
+				Values: func(values []reflect.Value, rand *rand.Rand) {
+					for i := 0; i < len(values); i++ {
+						var nextOp op
+						nextOp.t = opType(rand.Int() % 2)
+						switch nextOp.t {
+						case opRead:
+							if got, want := buffer.Readable(), availableForReading; got != uint32(want) {
+								t.Errorf("got %d, want %d readable bytes", got, want)
+							}
+							nextOp.n = rand.Intn(availableForReading + 1)
+							availableForReading -= nextOp.n
+						case opWrite:
+							sz := rand.Intn(bufferSize - availableForReading + 1)
+							availableForReading += sz
+							nextOp.p = make([]byte, sz)
+							rand.Read(nextOp.p)
+						default:
+							panic("unknown op type")
+						}
+						values[i] = reflect.ValueOf(&nextOp)
+					}
+				},
+			})
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	}
 }
